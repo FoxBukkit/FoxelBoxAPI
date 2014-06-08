d02e53359b81f630c087345d0074359322119d24
Doridian Draconia <mriq91@gmail.com>
2014-06-08 17:05:04 +0200

    Initial

---
+++ app/views/apiinfo.php
@@ -0,0 +1,12 @@
+{
+    "success": true,
+    "result": {
+        "name": "FoxelBox API",
+        "version": {
+            "current": "v1",
+            "available": [
+                "v1"
+            ]
+        }
+    }
+}
