ecb1587b866d319642abfc722aebb8c907dd35e2
Doridian Draconia <mriq91@gmail.com>
2014-07-13 19:47:54 -0400

    Make main page more consistent with v1 style

--- app/views/apiinfo.php
+++ app/views/apiinfo.php
@@ -1,12 +1,10 @@
 {
     "success": true,
-    "result": {
-        "name": "FoxelBox API",
-        "version": {
-            "current": "v1",
-            "available": [
-                "v1"
-            ]
-        }
-    }
+	"name": "FoxelBox API",
+	"version": {
+		"current": "v1",
+		"available": [
+			"v1"
+		]
+	}
 }
