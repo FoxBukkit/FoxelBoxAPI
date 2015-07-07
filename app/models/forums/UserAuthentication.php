83f02e8f9f9d720656f7388cfa609e47eea81b3a
Mark Dietzer <mriq91@gmail.com>
2015-07-07 19:24:15 +0200

    rm class

--- app/models/forums/UserAuthentication.php
+++ app/models/forums/UserAuthentication.php
@@ -4,12 +4,3 @@ class UserAuthentication extends Eloquent {
     protected $table = 'xf_user_authenticate';
     protected $primaryKey = 'user_id';
 }
-
-class XenForo_PasswordHash {
-    function CheckPassword($password, $stored_hash)
-    {
-        $hash = crypt($password, $stored_hash);
-
-        return $hash == $stored_hash;
-    }
-}
