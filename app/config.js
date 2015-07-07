8c3af6197d169a22ba0cde46b2f27837598a2371
Mark Dietzer <mriq91@gmail.com>
2015-07-07 16:59:08 +0200

    Change config system

---
+++ app/config.js
@@ -0,0 +1,5 @@
+var defaultConfig = require('./config/config.json');
+var env = process.env.NODE_ENV || 'development';
+var envConfig = require('./config/' + env + '.json');
+var _ = require('lodash');
+module.exports = _.extend(defaultConfig, envConfig);
