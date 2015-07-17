6b8c7b1bd23eb6f10ba167606f4b070ae723f6df
Mark Dietzer <mriq91@gmail.com>
2015-07-18 01:43:43 +0200

    Bad

--- app/daemon.js
+++ app/daemon.js
@@ -33,7 +33,7 @@ function trySubscribe () {
 function removeOldMessages () {
 	console.log('[REMOVEOLD]', 'start');
 	var minimalTime = util.getUnixTime() - 60;
-	redis.zrevrangeAsync(new Buffer('apiMessageCache'), 50, -1, new Buffer('withscores'))
+	redis.zrevrangeAsync(new Buffer('apiMessageCache'), 50, -1)
 	.filter(function (entry) {
 		return proto.ChatMessageOut.decode(entry).timestamp.lessThan(minimalTime);
 	})
