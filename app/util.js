98c304a96e87783bd01f924db344704b8333e912
Doridian <mriq91@gmail.com>
2015-09-14 22:41:26 +0200

    Fix UUID stuff

--- app/util.js
+++ app/util.js
@@ -27,13 +27,14 @@ module.exports.loadProtobufUUID = function (protobufUUID) {
 	dataview.setInt32(4, protobufUUID.msb.low, false);
 	dataview.setInt32(8, protobufUUID.lsb.high, false);
 	dataview.setInt32(12, protobufUUID.lsb.low, false);
-
-	return uuid.unparse(buffer);
+	var view = new Uint8Array(buffer);
+	return uuid.unparse(view);
 };
 
 module.exports.writeProtobufUUID = function (uuidUuid) {
 	var buffer = new ArrayBuffer(16); // 16 bytes = 128 bits
-	uuid.parse(uuidUuid, buffer, 0);
+	var view = new Uint8Array(buffer);
+	uuid.parse(uuidUuid, view, 0);
 	var dataview = new DataView(buffer);
 	return {
 		msb: new Long(dataview.getInt32(4), dataview.getInt32(0)),
